8404ed82ebe07d1c63eed386d9a7a238dddea5d3
Serhat Ece <134696090+serhatecee@users.noreply.github.com>
2024-07-31 10:58:38 +0300

    update-app

--- 13.TypeScript/src/App.tsx
+++ 13.TypeScript/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //! string , boolean , number
+  //! interface - type = kendi tipimizi oluşturmaya yarar.
+
+  type User = {
+    name : string,
+    age : number
+  }
+
+  interface User2{
+    name : string,
+    age : number
+  }
+
+  // let array :string[] = [];
+  // let array2 :User[] = [{name: "Ali", age: 19}];
+
+  const object1 : User = {
+    name : "Serhat",
+    age : 20
+  }
+
+  const object2 : User2 = {
+    name : "Harun",
+    age : 21
+  }
+
+  console.log(object1)
+  console.log(object2)
+
+
+
+
+  //! Function : Çok önemli!!!
+
+  function topla(a:number,b:number):number | string{
+    return a + b
+  }
+
+  const sonuc = topla(17,9);
+
+  console.log(sonuc)
+
+  // void : geriye bir şey döndürmeyen metottur.
+
+  function write(array : Array<string>) :void{
+    array.forEach((value:string)=>console.log(value))
+  }
+
+  let array: Array<string> = ["enes","ali","yagmur"]
+
+  write(array)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      TypeScript
+    </div>
   )
 }
 
